d203dc384030eab45f7a8f84712353e32999d0c7
Stephen <stephenma064@gmail.com>
2016-11-27 23:09:33 -0600

    Pick out basic chat

---
+++ ChatClient.java
@@ -0,0 +1,84 @@
+package assignment7;
+
+import java.io.*;
+import java.net.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ChatClient {
+	private JTextArea incoming;
+	private JTextField outgoing;
+	private BufferedReader reader;
+	private PrintWriter writer;
+	
+
+	public void run() throws Exception {
+		initView();
+		setUpNetworking();
+	}
+
+	private void initView() {
+		JFrame frame = new JFrame("Ludicrously Simple Chat Client");
+		JPanel mainPanel = new JPanel();
+		incoming = new JTextArea(15, 50);
+		incoming.setLineWrap(true);
+		incoming.setWrapStyleWord(true);
+		incoming.setEditable(false);
+		JScrollPane qScroller = new JScrollPane(incoming);
+		qScroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		qScroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
+		outgoing = new JTextField(20);
+		JButton sendButton = new JButton("Send");
+		sendButton.addActionListener(new SendButtonListener());
+		mainPanel.add(qScroller);
+		mainPanel.add(outgoing);
+		mainPanel.add(sendButton);
+		frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+		frame.setSize(650, 500);
+		frame.setVisible(true);
+
+	}
+
+	private void setUpNetworking() throws Exception {
+		@SuppressWarnings("resource")
+		Socket sock = new Socket("127.0.0.1", 4242);
+		InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
+		reader = new BufferedReader(streamReader);
+		writer = new PrintWriter(sock.getOutputStream());
+		System.out.println("networking established");
+		Thread readerThread = new Thread(new IncomingReader());
+		readerThread.start();
+	}
+
+	class SendButtonListener implements ActionListener {
+		public void actionPerformed(ActionEvent ev) {
+			writer.println(outgoing.getText());
+			writer.flush();
+			outgoing.setText("");
+			outgoing.requestFocus();
+		}
+	}
+
+	public static void main(String[] args) {
+		try {
+			new ChatClient().run();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	class IncomingReader implements Runnable {
+		public void run() {
+			String message;
+			try {
+				while ((message = reader.readLine()) != null) {
+					
+						incoming.append(message + "\n");
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+	}
+}
---
+++ ChatServer.java
@@ -0,0 +1,56 @@
+package assignment7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Observable;
+
+public class ChatServer extends Observable {
+	public static void main(String[] args) {
+		try {
+			new ChatServer().setUpNetworking();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void setUpNetworking() throws Exception {
+		@SuppressWarnings("resource")
+		ServerSocket serverSock = new ServerSocket(4242);
+		while (true) {
+			Socket clientSocket = serverSock.accept();
+			ClientObserver writer = new ClientObserver(clientSocket.getOutputStream());
+			Thread t = new Thread(new ClientHandler(clientSocket));
+			t.start();
+			this.addObserver(writer);
+			System.out.println("got a connection");
+		}
+	}
+	class ClientHandler implements Runnable {
+		private BufferedReader reader;
+
+		public ClientHandler(Socket clientSocket) {
+			Socket sock = clientSocket;
+			try {
+				reader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void run() {
+			String message;
+			try {
+				while ((message = reader.readLine()) != null) {
+					System.out.println("server read "+message);
+					setChanged();
+					notifyObservers(message);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
