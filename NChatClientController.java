5951d04bf91cb8e19ecb3a1fff0c4d685559d1a5
Stephen <stephenma064@gmail.com>
2016-11-30 20:53:16 -0600

    styles

--- NChatClientController.java
+++ NChatClientController.java
@@ -13,53 +13,55 @@ import javafx.scene.text.TextFlow;
 
 /**
  * Controller for the NChatClient.
+ * 
  * @author ericsu
  *
  */
 public class NChatClientController implements javafx.fxml.Initializable {
-	
+
 	private Color chatColor;
-	
+
 	private NChatClient chatClient;
-	
-    @FXML
-    private TextField chatInputField;
 
-    @FXML
-    private Button sendChatMessage;
+	@FXML
+	private TextField chatInputField;
 
-    @FXML
-    private TextArea mainChatDisplay;
-    
-    @FXML
-    private TextField userNameInputField;
-    
-    public TextArea getMainChatDisplay() {
-    	return this.mainChatDisplay;
-    }
-    
-    public void setChatClient(NChatClient client) {
-    	this.chatClient = client; 
-    }
-    
-    public String getChatInputMessage() {
-    	return this.chatInputField.getText();
-    }
-    
-    public void sendMessage() {
-    	String messageText = this.chatInputField.getText();
-    	this.chatClient.getChatWriter().println(messageText);
-    	this.chatClient.getChatWriter().flush();
-    	this.chatInputField.setText("");
-    	this.chatInputField.requestFocus();
-    }
+	@FXML
+	private Button sendChatMessage;
 
+	@FXML
+	private TextArea mainChatDisplay;
+
+	@FXML
+	private TextField userNameInputField;
+
+	public void setStyle() {
+		sendChatMessage.setStyle("-fx-background-color: rgb(14,122,254); -fx-text-fill: rgb(255,255,255);");
+	}
+
+	public TextArea getMainChatDisplay() {
+		return this.mainChatDisplay;
+	}
+
+	public void setChatClient(NChatClient client) {
+		this.chatClient = client;
+	}
+
+	public String getChatInputMessage() {
+		return this.chatInputField.getText();
+	}
+
+	public void sendMessage() {
+		String messageText = this.chatInputField.getText();
+		this.chatClient.getChatWriter().println(messageText);
+		this.chatClient.getChatWriter().flush();
+		this.chatInputField.setText("");
+		this.chatInputField.requestFocus();
+	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
-
-		
+		setStyle();
 	}
 
 }
